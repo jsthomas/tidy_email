5720230f7dcda560d87fc0418ae495fa453c6fbf
Joseph Thomas <jsthomas@protonmail.com>
2021-08-23 14:28:49 -0600

    Add tests for headers and parameters.

--- mailgun/test/test_tidy_email_mailgun.ml
+++ mailgun/test/test_tidy_email_mailgun.ml
@@ -6,14 +6,24 @@
 open Lwt.Infix
 
 (* For these tests the content of the configuration and the email
-   aren't especially important, there can just be one pair used across
+   aren't especially important. There can just be one pair used across
    all tests. *)
-let config : Tidy_email_mailgun.Mailgun.config =
-  { api_key = "12345"; base_url = "https://test.com/" }
 
-let email =
-  Tidy_email.Email.make ~sender:"sender@a.com" ~recipient:"receiver@b.com"
-    ~subject:"Some title." ~body:(Tidy_email.Email.Text "Some text here.")
+let api_key = "test_password"
+
+let config : Tidy_email_mailgun.Mailgun.config =
+  { api_key;
+    base_url = "https://api.com";
+  }
+
+let text_body = Tidy_email.Email.Text "Some text here."
+
+let email body =
+  Tidy_email.Email.make
+    ~sender:"sender@a.com"
+    ~recipient:"receiver@b.com"
+    ~subject:"Some title."
+    ~body
 
 let check_result =
   let testable =
@@ -26,34 +36,77 @@ let check_result =
     Alcotest.testable formatter eq in
   Alcotest.check testable
 
+let check_headers =
+  let testable =
+      let print h =
+        match h with
+        | None -> "None"
+        | Some h -> Cohttp.Header.to_string h
+      in
+      let eq oh1 oh2 = match oh1, oh2 with
+        | (None, None) -> true
+        | (Some h1, Some h2) -> (Cohttp.Header.compare h1 h2 == 0)
+        | _ -> false
+      in
+      let formatter ppf value = Format.pp_print_string ppf (print value) in
+      Alcotest.testable formatter eq in
+  Alcotest.check testable
+
+
 (* This mock allows us to represent different status/body combinations
-   that Mailgun's API might produce. *)
-let post_form status body ?headers ~params uri =
+   that Mailgun's API might produce. Once we bind in a response
+   status, response body, and expected post body, we're left with an
+   http_post_form function.*)
+let post_form (post_body: Tidy_email.Email.body) status body ?headers ~params uri =
   let open Cohttp in
   let response = Response.make ~status () in
   let body = Cohttp_lwt.Body.of_string body in
-  let header_str =
-    match headers with
-    | None -> "None"
-    | Some h -> Cohttp.Header.to_string h in
-  let render_parameter (k, vs) =
-    Printf.sprintf "%s : %s" k (String.concat "," vs) in
-  let param_str = String.concat "\n" (List.map render_parameter params) in
-  Printf.printf "Headers: %s" header_str;
-  Printf.printf "Params:\n %s" param_str;
-  Printf.printf "URI:\n %s" @@ Uri.to_string uri;
+  let expected_headers = Some (
+      Cohttp.Header.add_authorization
+        (Cohttp.Header.init ())
+        (`Basic ("api", api_key))
+    ) in
+  let additional_params =
+    match post_body with
+    | Text _ -> [ "text" ]
+    | Html _ -> [ "html" ]
+    | Mixed _ -> ["text"; "html"]
+  in
+  let expected_params = ["from"; "to"; "subject"] @ additional_params in
+  let actual_params = List.map fst params in
+  Alcotest.(check string) "The correct API endpoint is used." "https://api.com/messages" (Uri.to_string uri);
+  check_headers "A basic auth header is supplied." expected_headers headers;
+  Alcotest.(check (list string)) "The expected parameters are present." expected_params actual_params;
   Lwt.return (response, body)
+
 
 let test_bad_credentials _ () =
   (* When Mailgun replies with a 401 Unauthorized, because bad credentials
      were supplied, an email send fails with an error. *)
-  let client = post_form `Unauthorized "Forbidden" in
+  let client = post_form text_body `Unauthorized "Forbidden" in
   let sender = Tidy_email_mailgun.Mailgun.client_send client in
-  sender config email
+  email text_body
+  |> sender config
   >|= check_result "An error is produced." (Error "Forbidden")
 
-let ok_response =
-  {|
+
+let bad_data_response = {|
+{
+  "message": "'to' parameter is not a valid address. please check documentation"
+}
+|}
+
+let test_bad_data _ () =
+  (* When Mailgun replies with a 400 Bad Request, the function call
+     converts the response to an error. *)
+  let client = post_form text_body `Bad_request bad_data_response in
+  let sender = Tidy_email_mailgun.Mailgun.client_send client in
+  email text_body
+  |> sender config
+  >|= check_result "An error is produced." (Error bad_data_response)
+
+
+let ok_response = {|
 {
   "id": "<20210823170937.1.7C6EB132B58E7372@testdomain.com>",
   "message": "Queued. Thank you."
@@ -63,34 +116,42 @@ let ok_response =
 let test_success _ () =
   (* When Mailgun replies with a 200 OK, the function call produces an
      ok. *)
-  let client = post_form `OK ok_response in
+  let client = post_form text_body `OK ok_response in
   let sender = Tidy_email_mailgun.Mailgun.client_send client in
-  sender config email >|= check_result "An ok is produced." (Ok ())
+  email text_body |> sender config >|= check_result "An ok is produced." (Ok ())
 
-let bad_data_response =
-  {|
-{
-  "message": "'to' parameter is not a valid address. please check documentation"
-}
-|}
 
-let test_bad_data _ () =
-  (* When Mailgun replies with a 400 Bad Request, the function call
-     converts the response to an error. *)
-  let client = post_form `Bad_request bad_data_response in
+let test_success_html _ () =
+  (* The library sends an HTML email request with the parameters Mailgun expects. *)
+  let body = Tidy_email.Email.Html "Some html here." in
+  let client = post_form body `OK ok_response in
   let sender = Tidy_email_mailgun.Mailgun.client_send client in
-  sender config email
-  >|= check_result "An error is produced." (Error bad_data_response)
+  email body |> sender config >|= check_result "An ok is produced." (Ok ())
+
+
+let test_success_mixed _ () =
+  (* The library sends a mixed text/HTML email request with the parameters Mailgun expects. *)
+  let body = Tidy_email.Email.Mixed ("Some text here.", "Some html here.", None) in
+  let client = post_form body `OK ok_response in
+  let sender = Tidy_email_mailgun.Mailgun.client_send client in
+  email body |> sender config >|= check_result "An ok is produced." (Ok ())
+
 
 let () =
   let open Alcotest_lwt in
   Lwt_main.run
   @@ run "Mailgun Tests"
        [
-         ( "basic",
+         ("email errors",
            [
              test_case "Invalid Credentials" `Quick test_bad_credentials;
-             test_case "Success" `Quick test_success;
              test_case "Bad Data" `Quick test_bad_data;
+
            ] );
+         ("email success cases",
+          [
+            test_case "Success - Text" `Quick test_success;
+            test_case "Success - HTML" `Quick test_success_html;
+            test_case "Success - Mixed" `Quick test_success_mixed;
+          ]);
        ]
