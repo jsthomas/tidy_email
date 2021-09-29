940c8a7879847285a8ae66420744adb98e956c24
Joseph Thomas <jsthomas@protonmail.com>
2021-09-28 19:57:46 -0600

    Clean-up module definitions a bit.

--- mailgun/src/tidy_email_mailgun.ml
+++ mailgun/src/tidy_email_mailgun.ml
@@ -1,17 +1,21 @@
-module Header = Cohttp.Header
-module Body = Cohttp_lwt.Body
 module Auth = Cohttp.Auth
+module Body = Cohttp_lwt.Body
+module Header = Cohttp.Header
+module Response = Cohttp.Response
+
 
 type config = {
   api_key : string;
   base_url : string;
 }
 
+
 type http_post =
-  ?body:Cohttp_lwt.Body.t ->
-  ?headers:Cohttp.Header.t ->
+  ?body:Body.t ->
+  ?headers:Header.t ->
   Uri.t ->
-  (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t
+  (Response.t * Body.t) Lwt.t
+
 
 let client_send (post : http_post) (conf : config) (e : Tidy_email.Email.t) =
   let param =
@@ -25,17 +29,18 @@ let client_send (post : http_post) (conf : config) (e : Tidy_email.Email.t) =
   let cred = `Basic ("api", conf.api_key) in
   let uri = conf.base_url ^ "/messages" |> Uri.of_string in
   let headers =
-    Cohttp.Header.of_list
+    Header.of_list
       [
         ("content-type", "application/x-www-form-urlencoded");
         ("authorization", Auth.string_of_credential cred);
       ] in
   let body = Body.of_string (Uri.encoded_of_query params) in
   let%lwt response, resp_body = post ~headers ~body uri in
-  let%lwt body_content = Cohttp_lwt.Body.to_string resp_body in
-  match Cohttp.Response.status response with
+  let%lwt body_content = Body.to_string resp_body in
+  match Response.status response with
   | `OK -> Lwt.return_ok ()
   | _ -> Lwt.return_error body_content
+
 
 let send =
   client_send
